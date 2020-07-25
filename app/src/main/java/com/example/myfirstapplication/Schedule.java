81d18530a61bffba89c081773e23c278dcd852d5
Ying Qi <yingqi0616@gmail.com>
2020-07-25 20:49:39 +0800

    25/7/20 Debugged

--- app/src/main/java/com/example/myfirstapplication/Schedule.java
+++ app/src/main/java/com/example/myfirstapplication/Schedule.java
@@ -1,5 +1,9 @@
 package com.example.myfirstapplication;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
@@ -18,6 +22,7 @@ class Schedule {
 
     //return type is boolean
     //true if the scheduling is successful and vice versa
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public boolean scheduling(List<Integer> freeDay) {
         FilterFreeDay filterLesson = new FilterFreeDay(this.listOfLessons, freeDay, this.timetable);
         List<AllLesson> filteredList = filterLesson.filter();
@@ -30,8 +35,161 @@ class Schedule {
         }
 
         for (AllLesson listLessons : filteredList) {
-            //hasAdded indicates whether a lecture has been added into the tt
+            List<Lesson> newLessonList;
+            boolean isPossible = false;
             boolean hasAdded = false;
+
+            if (listLessons.getIsGrouped()) {
+                for (Lesson newLesson : listLessons.getAllTimings()) {
+                    newLessonList = listLessons.findLesson(newLesson.getNum());
+                    for (Lesson lesson : newLessonList) {
+                        if (!this.timetable.check(lesson)) {
+                            isPossible = false;
+                            break;
+                        } else {
+                            isPossible = true;
+                        }
+                    }
+                }
+
+                if (isPossible) {
+                    hasAdded = true;
+                    for (Lesson newLesson : listLessons.getAllTimings()) {
+                        this.timetable.add(newLesson);
+                    }
+                } else {
+                    hasAdded = false;
+                }
+            } else {
+
+                for (Lesson newLesson : listLessons.getAllTimings()) {
+                    if (!this.timetable.check(newLesson)) {
+                        hasAdded = false;
+                    } else {
+                        this.timetable.add(newLesson);
+                        hasAdded = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!hasAdded) {
+                for (AllLesson ogLesson : this.listOfLessons) {
+                    boolean added = false;
+                    if (ogLesson.getCode().equals(listLessons.getCode())) {
+                        //single lesson
+                        if (!ogLesson.getIsGrouped()) {
+                            for (Lesson lesson : ogLesson.getAllTimings()) {
+                                if (this.timetable.check(lesson)) {
+                                    added = true;
+                                    this.timetable.add(lesson);
+                                    break;
+                                }
+                            }
+                            if (!added) {
+                                System.out.println("error 6");
+                                this.timetable.setPossibleFreeDay(noFreeDay);
+                                return false;
+
+                            } else { //added
+                                ArrayList<Integer> removeDays = new ArrayList<>();
+                                for (int day : this.timetable.getPossibleFreeDay()) {
+                                    if (!this.timetable.getFreeDay().contains(day)) {
+                                        removeDays.add(day);
+                                    }
+                                }
+
+                                for (int day : removeDays) {
+                                    this.timetable.removeFreeDay(day);
+                                }
+                                continue;
+                                //return true;
+                            }
+
+                            //multiple lessons
+                        } else {
+                            List<Lesson> bestLessonList = new ArrayList<>();
+                            int leastDays = 7;
+                            int currDays = 0;
+                            isPossible = false;
+                            List<Lesson> currentLessonList;
+
+                            for (Lesson lesson : ogLesson.getAllTimings()) {
+                                currentLessonList = ogLesson.findLesson(lesson.getNum());
+                                for (Lesson checkLesson : currentLessonList) {
+                                    if (!this.timetable.check(checkLesson)) {
+                                        isPossible = false;
+                                        break;
+                                    } else {
+                                        isPossible = true;
+                                    }
+                                }
+
+                                if (isPossible) {
+                                    List<Integer> days = this.timetable.getFreeDay();
+                                    for (Lesson checkedLesson : currentLessonList) {
+                                        if (days.contains(checkedLesson.getDay())) {
+                                            currDays++; //least the better, means more freeDay
+                                        }
+                                    }
+                                    if (currDays < leastDays) {
+                                        bestLessonList = currentLessonList;
+                                    }
+                                } // else continue to look for other possible lessons
+                            }
+
+                            if (bestLessonList.isEmpty()) {
+                                added = false;
+                            } else {
+                                for (Lesson lesson : bestLessonList) {
+                                    this.timetable.add(lesson);
+                                }
+                                added = true;
+                            }
+                        }
+
+                        if (!added) {
+                            System.out.println("error 7");
+                            this.timetable.setPossibleFreeDay(noFreeDay);
+                            return false;
+
+                        } else { //added
+                            ArrayList<Integer> removeDays = new ArrayList<>();
+                            for (int day : this.timetable.getPossibleFreeDay()) {
+                                if (!this.timetable.getFreeDay().contains(day)) {
+                                    removeDays.add(day);
+                                }
+                            }
+
+                            for (int day : removeDays) {
+                                this.timetable.removeFreeDay(day);
+                            }
+                            //return true;
+                        }
+
+
+                    }
+                }
+            } else { //hasadded
+                ArrayList<Integer> removeDays = new ArrayList<>();
+                for (int day : this.timetable.getPossibleFreeDay()) {
+                    if (!this.timetable.getFreeDay().contains(day)) {
+                        removeDays.add(day);
+                    }
+                }
+
+                for (int day : removeDays) {
+                    this.timetable.removeFreeDay(day);
+                }
+                //return true;
+            }
+        }
+        return true;
+    }
+
+}
+            //hasAdded indicates whether a lecture has been added into the tt
+ /*           boolean hasAdded = false;
             String lessonNum = "";
             Lesson addedLesson = null;
             //getAlltimings == possibleTime() cuz alr filtered in lesson Sim
@@ -92,7 +250,7 @@ class Schedule {
                                             added = false;
                                         }
                                     } else {
-                                        break;
+                                        continue;
                                     }
                                 } else {
                                     if (this.timetable.check(lesson)) {
@@ -131,5 +289,5 @@ class Schedule {
 
         return true;
     }
-}
+}*/
 
